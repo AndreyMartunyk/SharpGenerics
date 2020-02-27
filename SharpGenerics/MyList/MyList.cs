964caa677277a12872c3c371e6575913902cff46
Andrey Martunyk <aniks.zoom@gmail.com>
2020-02-27 19:54:40 +0200

    Initial commit

---
+++ SharpGenerics/MyList/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class MyList<T>
+    {
+        public int Length { get; private set; }
+        private T[] _arr;
+
+        public MyList()
+        {
+            _arr = new T[10];
+            Length = 0;
+        }
+
+        private void increaseArray()
+        {
+            T[] newArray = new T[_arr]
+        }
+
+    }
+}
---
+++ SharpGenerics/SharpGenerics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGenerics
+{
+    class Program
+    {
+        // 1)Создайте класс MyClass<T>, содержащий статический фабричный метод - T FacrotyMethod(), который
+        //будет порождать экземпляры типа, указанного в качестве параметра типа(указателя места заполнения типом –
+        //Т).
+        static void Main(string[] args)
+        {
+            Foo foo = MyClass<Foo>.FactoryMethod();
+        }
+    }
+
+    class Foo
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    class MyClass<T> where T:new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
